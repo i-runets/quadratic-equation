6dcd524ee9deb5b54bf222f8821791d19f2bbc69
seafiv <runecivan@gmail.com>
2018-02-20 04:26:11 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  var a, b, c, x1, x2, d;
+  var result = [];
+  var elements = equation.split(' ');
+  a = Number(elements[0]);
+  b = Number(elements.slice(3, 5).join(''));
+  c = Number(elements.slice(7, 9).join(''));
+  d = Math.pow(b, 2) - 4 * (a) * (c);
+  x1 = Math.round((-b + Math.sqrt(d)) / (2 * a));
+  x2 = Math.round((-b - Math.sqrt(d)) / (2 * a));
+  if (x1 < x2) {
+    result[0] = x1;
+    result[1] = x2;
+  }
+  else {
+    result[0] = x2;
+    result[1] = x1;
+  }
+  return result;
 }
